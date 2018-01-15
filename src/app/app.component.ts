ea7697ebe5e1287f4430fa73d612400a1b7c45e1
ninjadev1030 <dalianwang1030@yahoo.com>
2018-01-15 23:47:38 +0800

    update component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -12,7 +12,8 @@ export class AppComponent {
     { id: 1, name: 'Apple', des: 'This is an Apple.', amount: 5 },
     { id: 2, name: 'Banana', des: 'This is a Banana.', amount: 10 },
     { id: 3, name: 'Peach', des: 'This is a Peach.', amount: 4 },
-    { id: 4, name: 'Pear', des: 'This is a Pear.', amount: 7 }
+    { id: 4, name: 'Pear', des: 'This is a Pear.', amount: 7 },
+    { id: 5, name: 'Grape', des: 'This is a Grape.', amount: 8}
   ];
 
   toggleItemInArr(arr, item) {
@@ -23,9 +24,6 @@ export class AppComponent {
   clickTable(fruit: any, event) {
     if (!event.ctrlKey) {
       this.selectedFruitArray = [];
-      // } else if (!event.shiftKey) {
-      //   this.selectedFruitArray = [];
-      // }
     }
 
     this.toggleItemInArr(this.selectedFruitArray, fruit);
